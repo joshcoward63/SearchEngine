4117ae25a88a88c3da2a84a24efa0d5d37f2ae94
joshcoward63 <49047401+joshcoward63@users.noreply.github.com>
2021-03-24 17:42:56 -0600

    Closes Create function to parse body tags #36 (#44)

--- Parser.py
+++ Parser.py
@@ -5,13 +5,21 @@ from lxml import html
 import requests
 
 import requests
-page = requests.get('http://examplesite.com')
+page = requests.get("https://www.boisestate.edu/coen-cs/")
 contents = page.content
 
-page = requests.get('http://example.com/ex/somewebpage.html')
-tree = html.fromstring(page.content)
+# page = requests.get('http://example.com/ex/somewebpage.html')
+# tree = html.fromstring(page.content)
 
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(contents, 'html.parser')
 soup.find_all('a')
 
+
+"""Function is used to get body tag information"""
+def get_body_tags(page):
+    soup = BeautifulSoup(page, "html.parser")
+    body_text = soup.find("body").text
+    return body_text
+
+# print(get_body_tags(contents))
