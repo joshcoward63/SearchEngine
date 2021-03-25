53380a96bb490ecff8733bdc19a1f6521b931995
joshcoward63 <49047401+joshcoward63@users.noreply.github.com>
2021-03-24 22:09:05 -0600

    Create function to parse unordered list tags #37 (#46)

--- Parser.py
+++ Parser.py
@@ -3,9 +3,9 @@ from selenium.webdriver.common.keys import Keys
 
 from lxml import html
 import requests
-
+import re
 import requests
-page = requests.get("https://www.boisestate.edu/coen-cs/")
+page = requests.get("https://www.boisestate.edu/coen-cs/people/faculty/")
 contents = page.content
 
 # page = requests.get('http://example.com/ex/somewebpage.html')
@@ -22,4 +22,13 @@ def get_body_tags(page):
     body_text = soup.find("body").text
     return body_text
 
-# print(get_body_tags(contents))
+"""Function is used to get information stored in an unordered list from webpage"""
+def get_ul_tags(page):
+    soup = BeautifulSoup(page, "html.parser")
+    ul = soup.find_all("ul")
+    unordered_list = []
+    for wrapper in ul:
+        unordered_list.append(' '.join(wrapper.text.replace('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n',"@@@@@@").replace('\n',' ').replace('\t',"").split()).split("@@@@@@"))
+    return unordered_list
+
+# print(get_ul_tags(contents))
