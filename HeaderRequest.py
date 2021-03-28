22f9cb12aab3accf0133aed2d72cb4e21ef8534f
ZahraRahmani2 <64114569+ZahraRahmani2@users.noreply.github.com>
2021-03-27 19:34:14 -0600

    Create HeaderRequest.py (#47)

---
+++ HeaderRequest.py
@@ -0,0 +1,18 @@
+
+# import all the libraries
+import requests
+from bs4 import BeautifulSoup
+  
+# target url
+url = 'https://www.somedomain.org/'
+  
+# requests instance
+reqs = requests.get(url)
+  
+# used BeaitifulSoup module
+soup = BeautifulSoup(reqs.text, 'html.parser')
+  
+# shows the title
+print("Title of the website is : ")
+for title in soup.find_all('title'):
+    print(title.get_text())
