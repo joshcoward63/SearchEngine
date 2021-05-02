1433a515cc061aeab21006505021e1f87853ff1c
Josh <joshcoward63@yahoo.com>
2021-05-01 21:54:25 -0600

    Closes  #53

--- Parser.py
+++ Parser.py
@@ -6,30 +6,45 @@ from lxml import html
 import requests
 import re
 import requests
-page = requests.get("https://www.boisestate.edu/coen-cs/people/faculty/")
-contents = page.content
+# page = requests.get("https://www.boisestate.edu/coen-cs/people/faculty/")
+# contents = page.content
 
 # page = requests.get('http://example.com/ex/somewebpage.html')
 # tree = html.fromstring(page.content)
 
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(contents, 'html.parser')
-soup.find_all('a')
+# soup = BeautifulSoup(contents, 'html.parser')
+# soup.find_all('a')
 
+"""Function is used to get title tag information"""
+def get_title_tags(page):
+    contents = page.content
+    soup = BeautifulSoup(contents, "html.parser")
+    try:
+        title_text = soup.find("title").text
+    except:
+        title_text = ""
+    return title_text
 
 """Function is used to get body tag information"""
 def get_body_tags(page):
-    soup = BeautifulSoup(page, "html.parser")
-    body_text = soup.find("body").text
+    contents = page.content
+
+    soup = BeautifulSoup(contents, "html.parser")
+    try:
+        body_text = soup.find("body").text
+    except:
+        body_text = ""
     return body_text
 
 """Function is used to get information stored in an unordered list from webpage"""
 def get_ul_tags(page):
-    soup = BeautifulSoup(page, "html.parser")
+    contents = page.content
+    soup = BeautifulSoup(contents, "html.parser")
     ul = soup.find_all("ul")
     unordered_list = []
     for wrapper in ul:
         unordered_list.append(' '.join(wrapper.text.replace('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n',"@@@@@@").replace('\n',' ').replace('\t',"").split()).split("@@@@@@"))
     return unordered_list
 
-# print(get_ul_tags(contents))
+# print(get_title_tags(contents))
